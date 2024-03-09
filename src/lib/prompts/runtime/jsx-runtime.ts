af37b9c0d0103216e4020d4e829675bde4d04d02
René <renejfc@gmail.com>
2024-03-09 06:56:45 +0000

    feat(runtime): improve impl + edge case

--- src/lib/prompts/runtime/jsx-runtime.ts
+++ src/lib/prompts/runtime/jsx-runtime.ts
@@ -1,5 +1,7 @@
+import { childrenToArr } from "./common"
 import type { JSXSignature } from "./types"
 
 export function jsx(component: JSXSignature["component"], props: JSXSignature["props"]): any {
+  props.children = childrenToArr(props.children)
   return component(props)
 }
