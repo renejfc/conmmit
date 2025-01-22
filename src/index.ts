3c711e000061ff5600b7c5e88e2d94f4caaac18b
René <renejfc@gmail.com>
2025-01-22 21:11:20 +0100

    revert: revert last test change

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const args = parseArgs([
   [["add-all", "A"], "Add all changes to the commit"],
 ])
 
-intro(c.bold(c.bgCyan("TEST PREVIEW COMMIT")))
+intro(c.bold(c.bgCyan("CONMMIT")))
 
 const addResults = (args.get("add") && (await addPrompt())) || []
 const commitResults = await commitPrompt()
