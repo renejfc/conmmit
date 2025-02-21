eb070c5906971d2f0ee4992f8ce7ce0d030adc42
René <renejfc@gmail.com>
2025-02-21 02:37:00 +0100

    ci: actions stuff (#6)
    * ci: add version to pr comment
    
    * refactor: change format
    
    * fix: wip fix
    
    * fix: wip fix
    
    * ci: add test workflow
    
    * chore: test change
    
    * chore: anothe test change

--- .github/scripts/preview-comment.js
+++ .github/scripts/preview-comment.js
@@ -14,7 +14,7 @@ const generateUrls = ({ sha, owner, repo, commitUrl = null, prNumber = null }) =
   }
 }
 
-const generateCommentBody = ({ sha, owner, repo, commitUrl, prNumber }) => {
+const generateCommentBody = ({ sha, owner, repo, commitUrl, prNumber, version }) => {
   const { shortSha, shortUrl } = generateUrls({ sha, owner, repo, commitUrl, prNumber })
 
   return `${BOT_COMMENT_IDENTIFIER}
@@ -24,7 +24,14 @@ Try this version:
 bun add -g ${shortUrl}
 \`\`\`
 
-###### _[${shortSha}](${commitUrl})_`
+###### _[v${version} — ${shortSha}](${commitUrl})_`
+}
+
+const getNextVersion = (existingComment) => {
+  if (!existingComment?.body) return 1
+  const match = existingComment.body.match(/v(\d+) —/)
+  const currentVersion = Number.parseInt(match[1], 10)
+  return currentVersion + 1
 }
 
 const findBotComment = async ({ github, context, issueNumber }) => {
@@ -96,7 +103,10 @@ const handlePush = async ({ logger, github, context, body, output, commitUrl }) 
 
 export async function run(github, context, core) {
   const output = JSON.parse(readFileSync("output.json", "utf8"))
-  const sha = context.eventName === "pull_request" ? context.payload.pull_request.head.sha : context.payload.after
+  const sha =
+    context.eventName === "pull_request"
+      ? context.payload.pull_request.head.sha
+      : context.payload.after
   const prNumber = context.eventName === "pull_request" ? context.payload.pull_request.number : null
 
   const { commitUrl } = generateUrls({
@@ -106,12 +116,19 @@ export async function run(github, context, core) {
     prNumber,
   })
 
+  const existingComment = await findBotComment({
+    github,
+    context,
+    issueNumber: prNumber || context.issue.number,
+  })
+
   const body = generateCommentBody({
     sha,
     owner: context.repo.owner,
     repo: context.repo.repo,
     commitUrl,
     prNumber,
+    version: getNextVersion(existingComment),
   })
 
   const handlers = {
